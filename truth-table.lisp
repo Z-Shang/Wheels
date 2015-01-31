930647671e6262af40e6d9556bf15928ebabaa05
Z-Shang <shangzhanlin@gmail.com>
2015-01-31 18:12:36 +0000

    done gen-truth-table

--- truth-table.lisp
+++ truth-table.lisp
@@ -26,15 +26,59 @@
     ((equal sym 'f)
      't)))
 
-(defun l-and (syma symb)
-  (if (and (equal syma 't)
-           (equal symb 't))
+(defun l-and (&rest syms)
+  (if (member 'f syms)
+      'f
+      't))
+
+(defun l-or (&rest syms)
+  (if (member 't syms)
       't
       'f))
 
-(defun l-or (syma symb)
-  (if (or (equal syma 't)
-          (equal symb 't))
-      't))
+(defun join-two-lists (a b)
+  (if (not (equal (length a)
+                  (length b)))
+      'ERR-LENGTH-NOT-EQUAL
+      (if (null a)
+          nil
+          (cons (cons (car a) (car b)) (join-two-lists (cdr a) (cdr b))))))
 
-(defun )
+(defun logic-to-string (l)
+  (cond
+    ((equal l 't)
+     "T")
+    ((equal l 'f)
+     "F")
+    ((equal 'l-or (car l))
+     (format nil "~@{~A ~^+ ~}" (mapcar #'logic-to-string (cdr l))))
+    ((equal 'l-and (car l))
+     (format nil "~@{~A ~^* ~}" (mapcar #'logic-to-string (cdr l))))
+    ((equal 'l-not (car l))
+     (format nil "(~A)'" (logic-to-string (cadr l))))))
+
+(defun lookup (sym env)
+  (if (null env)
+      nil
+      (if (equal sym (caar env))
+          (cdr (car env))
+          (lookup sym (cdr env)))))
+
+(defun eval-logic (arglst exp)
+;  (format *standard-output* "~%~A~%~A~%" arglst exp)
+  (if (atom exp)
+      (lookup exp arglst)
+      (let ((vlst
+             (loop for a in (cdr exp)
+                collect (eval-logic arglst a)))
+            (f (car exp)))
+        (apply f vlst))))
+
+(defun gen-truth-table (args exps)
+  (let ((alst (apply #'cartesian-product (make-list (length args) :initial-element '(t f)))))
+    (format *standard-output* "~{~A~T~T~T~}=>~T~T~T~{~A~T~T~T~}~%" args exps)
+    (loop for l in alst
+       do (format *standard-output* "~{~A~T~T~T~}=>~T~T~T~{~A~T~T~T~}~%"
+                  l
+                  (mapcar #'(lambda (x) (eval-logic (join-two-lists args l)
+                                               x)) exps)))))
